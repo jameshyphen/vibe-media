f2595e9ff1741f22a0928735f577286f29aedbda
Dzhem Aptula <dzhem.aptula@gmail.com>
2025-04-20 16:38:55 +0200

    feat: add vibe.local to allowed hosts

--- social_media/settings.py
+++ social_media/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-4i=co187&$6ta@9qt)5ey2!#ov($(x0)h(gt2xy724jhxjob#g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'vibe.local']
 
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:43439', 'http://127.0.0.1:37571']
 
